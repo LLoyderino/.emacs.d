8abfc76d68510472d03aabfa088840d752670e1c
LLoyderino <adrijan.bjedov@gmail.com>
2022-07-03 18:01:08 +0200

    show mode letter rather than just the dot

--- lisp/init-doom-modeline.el
+++ lisp/init-doom-modeline.el
@@ -1,5 +1,7 @@
 (use-package doom-modeline
   :ensure all-the-icons
-  :init (doom-modeline-mode 1))
+  :init (doom-modeline-mode 1)
+  :config
+  (setq doom-modeline-modal-icon nil))
 
 (provide 'init-doom-modeline)
