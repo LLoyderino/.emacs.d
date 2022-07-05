91670605581dae722ede8fe88df53618917de58b
LLoyderino <adrijan.bjedov@gmail.com>
2022-07-05 12:29:46 +0200

    now supporting basic <SPC> keybinds

--- lisp/init-keybinds.el
+++ lisp/init-keybinds.el
@@ -8,4 +8,9 @@
 ;; Write tilde as M-\
 (global-set-key (kbd "M-\\") (lambda () (interactive) (insert "~")))
 
+;; <SPC> Keybinds
+(evil-leader/set-key "f s" 'save-buffer)
+(evil-leader/set-key "p f" 'project-find-file)
+(evil-leader/set-key "g" 'magit-status)
+
 (provide 'init-keybinds)
