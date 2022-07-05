7e5e92fb71042e85f644465ed1becdfac9fa9b06
LLoyderino <adrijan.bjedov@gmail.com>
2022-07-05 20:21:33 +0200

    idk keybinds I guess

--- lisp/init-keybinds.el
+++ lisp/init-keybinds.el
@@ -3,7 +3,7 @@
   (setq w32-lwindow-modifier 'super))
 
 ;; Paste Alt + Win + V
-(global-set-key (kbd "M-s-v") 'yank)
+(global-set-key (kbd "s-v") 'yank)
 
 ;; Write tilde as M-\
 (global-set-key (kbd "M-\\") (lambda () (interactive) (insert "~")))
@@ -11,6 +11,11 @@
 ;; <SPC> Keybinds
 (evil-leader/set-key "f s" 'save-buffer)
 (evil-leader/set-key "p f" 'project-find-file)
-(evil-leader/set-key "g" 'magit-status)
+
+;; Git - Magit
+(evil-leader/set-key "g g" 'magit-status)
+
+;; Buffers
+(evil-leader/set-key "b b" 'switch-to-buffer)
 
 (provide 'init-keybinds)
