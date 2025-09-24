0e83b6d8bf24ac59f16ddd69d12ac4e08571cd25
LLoyderino <adrijan.bjedov@gmail.com>
2025-09-24 11:40:04 +0200

    Lots of new changes:
    - Made .gitignore allow by default (rather than manually specifying what I want)
    - Dropped pre-built (non-versioned) TS grammars and use built-in ones
    - Upload custom snippets (lol 1)

--- init.el
+++ init.el
@@ -150,8 +150,10 @@
 
 ;; Treesitter grammars
 (setq treesit-language-source-alist
-      '((php "https://github.com/tree-sitter/tree-sitter-php" "v0.23.12" "php/src")
-        (phpdoc "https://github.com/claytonrcarter/tree-sitter-phpdoc" "v0.1.6")))
+      '((java "https://github.com/tree-sitter/tree-sitter-java" "v0.23.5")
+        (php "https://github.com/tree-sitter/tree-sitter-php" "v0.23.12" "php/src")
+        (phpdoc "https://github.com/claytonrcarter/tree-sitter-phpdoc" "v0.1.6")
+        (nix "https://github.com/nix-community/tree-sitter-nix" "v0.3.0")))
 
 ;; PHP
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-ts-mode))
