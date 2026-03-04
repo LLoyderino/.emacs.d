3fda87ab8eba13b2a45b3fbbb75b42a8458a1995
LLoyderino <adrijan.bjedov@gmail.com>
2026-03-04 21:24:24 +0100

    Removed mail configuration
    Not really using it

--- init.el
+++ init.el
@@ -107,14 +107,6 @@
   :config
   (org-roam-db-autosync-mode))
 
-;; Mail
-(load (locate-user-emacs-file "secrets.el"))                  ; Full-name and Mail address
-(setq gnus-select-method '(nnimap "imap.gmail.com")
-      message-send-mail-function 'smtpmail-send-it
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      gnus-message-archive-method '(nnimap "imap.gmail.com")
-      gnus-message-archive-group "[Gmail]/Sent Mail")         ; Gmail configuration
-
 ;; Catppuccin theme
 (load (locate-user-emacs-file "theme.el"))
 
