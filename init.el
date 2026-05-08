2f67cb19e75b7243ad0bf57c97591c71af5ac564
LLoyderino <adrijan.bjedov@gmail.com>
2026-05-08 22:21:27 +0200

    Removed yasnippet

--- init.el
+++ init.el
@@ -156,17 +156,6 @@
 (define-key flymake-mode-map (kbd "M-n") 'flymake-goto-next-error)
 (define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error)
 
-;; Snippets
-(use-package yasnippet
-  :hook ((text-mode
-          prog-mode
-          conf-mode
-          snippet-mode) . yas-minor-mode)
-  :init (setq yas-snippet-dir (locate-user-emacs-file "snippets")))
-
-(use-package yasnippet-snippets) ; Third party snippets, could replace
-                                 ; them with my own one day...
-
 ;; Treesitter grammars
 (setq treesit-language-source-alist
       '((css "https://github.com/tree-sitter/tree-sitter-css" "v0.23.2")
